04d5c2e283e7673a44502c8d4628ff04f0499deb
Neel Kotichintala <neeltherb@gmail.com>
2026-09-03 18:44:47 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+         // possible eatings rates will be from 1 to max(piles)
+         // use binary search on the eating rates array
+         // if current eating rate works go to left half
+         // if current eating rate doesnt work, go to right half
+        
+        int max = 0;
+        for (int pile: piles) {
+            if (pile > max) {
+                max = pile;
+            }
+        }
+
+        int l = 1;
+        int r = max;
+
+        int min = 1;
+
+        while (l <= r) {
+            int m = (r + l) / 2;
+
+            int time = 0;
+            for (int pile: piles) {
+                time += (pile + m - 1) / m;
+            }
+
+            if (time <= h) {
+                r = m - 1;
+                min = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return min;
+    }
+}
